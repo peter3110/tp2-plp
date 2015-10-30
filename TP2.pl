fb340a8db4683b45ef4d41a7240d5ce316ea4823
Pedro Rodriguez <pedro@Pedros-MacBook-Pro.local>
2015-10-30 18:27:43 -0300

    ahi esta hecho el codigo masomenos de la funcion de programacion dinamica... igual, no anda. fijense si entienden la idea de lo que puse, y asi la rehacemos o debuggeamos

--- TP2.pl
+++ TP2.pl
@@ -6,7 +6,9 @@
 
 % listaNats(+LInf,+LSup,?Nats), que unifica la lista Nats con los naturales en el rango [LInf, LSup], o una lista vacía si LSup < LInf.
 
-% Comentario: para que [H|T] sea la cabeza de Nats[LInf,LSup], si o si debe unificar con LInf 
+% Comentario: como caso base, decimos que en un intervalo vacio no hay solucion
+% 	si T es la solucion para listaNats entre H+1 y LSup, entonces [H|T] es solucion para listaNats
+% 	entre H y LSup
 listaNats(LInf,LSup,[])    :- LInf > LSup.
 listaNats(LInf,LSup,[H|T]) :- LInf =< LSup, H is LInf, Hm1 is H+1, listaNats(Hm1,LSup,T).
 
@@ -27,14 +29,14 @@ nPiezasDeCada(C,[T1|T],[pieza(T1,C)|P]) :- nPiezasDeCada(C,T,P).
 % resumenPiezas(+SecPiezas, -Piezas), que permite instanciar Piezas con la lista de
 % piezas incluidas en SecPiezas. 
 
-% Comentario: i) si la pieza T1 no esta en la solucion de S, agrego una pieza de tipo T1 a 
-%				 mi solucion, P.
+% Comentario: i) si la pieza T1 no esta en la solucion de S (O), agrego una (1) pieza de tipo T1 a 
+%				 dicha solucion O y de esta forma se arma la solucion final P.
 %			 ii) si la pieza T1 ya estaba en la solucion de S, sumo 1 a la cantidad de piezas
 %				 de tipo T1 que nos brinda la solucion S, y esta es mi nueva solucion, P.				 
 resumenPiezas([], []).
 resumenPiezas([T1|S], P) :- resumenPiezas(S,O), not(append(_,[pieza(T1,_)|_],O)),
 							append([pieza(T1,1)],O,P).
-resumenPiezas([T1|S], P) :- resumenPiezas(S,O), append(A,[pieza(T1,C)  |B],O), Cm1 is C+1, 
+resumenPiezas([T1|S], P) :- resumenPiezas(S,O), append(A,[pieza(T1,C)|B],O), Cm1 is C+1, 
 							append(A,[pieza(T1,Cm1)|B],P).
  							
 
@@ -49,14 +51,16 @@ resumenPiezas([T1|S], P) :- resumenPiezas(S,O), append(A,[pieza(T1,C)  |B],O), C
 %  cuyos valores suman Total. Aquí no se pide controlar que la cantidad de cada pieza
 %  esté acorde con la disponibilidad.
 
-% Comentario: si me pasan un nro <=0, no se podra encontrar una lista de piezas que verifique.
-%			  si no, busco en P piezas que podrian llegar a contribuir a una posible solucion,
-%			  y me fijo si de esta forma se genera una solucion factible. Si es asi, la agrego.
-%			  Como se pide en el enunciado, no tomo en cuenta disponibilidad acotada de piezas.
+% Comentario: si me pasan un nro <=0, no se podra encontrar una lista de piezas que verifique, pues
+%			  estamos trabajando con naturales.
+%			  si no, busco que en P existan piezas (X) que contribuyen a una posible solucion
+%			  factible. Dichas piezas X van a contribuir a la solucion S final.
+%			  Al no tener en cuenta la cantidad disponible de piezas tipo X que hay en P, no 
+%			  estoy tomando en cuenta que pueda haber disponibilidad acotada de piezas.
 
 generar(Tot,_,[]):- Tot =< 0.
 generar(Tot,P,S) :- append(_,[pieza(X,_)|_],P), Tot2 is Tot-X, Tot2 >= 0,
-					generar(Tot2,P,H), append(H,[pieza(X,1)],S).
+					generar(Tot2,P,H), append(H,[X],S).
 
 
 %%% Ejercicio 5 
@@ -67,14 +71,12 @@ generar(Tot,P,S) :- append(_,[pieza(X,_)|_],P), Tot2 is Tot-X, Tot2 >= 0,
 % Comentario: chequeo que para cada pieza utilizada en S se verifique que se utilizan como mucho
 %			  la cantidad de piezas de ese tipo que hay disponibles en P. Corto la recursion cuando
 %			  ya probe todas las combinaciones posibles.
-%			  Asumo que su utilizo la funcion "generar" para obtener la lista "Solucion", y hago uso
-%			  del hecho que en dicha lista la cantidad utilizada de cada pieza es siempre 1.
+%			  Asumo que S fue construido a partir de "generar": S = [X1,X2,X3,X3,..]
+%			  Observacion: corto a recursion en el arbol para no repetir soluciones iguales
 
 cumpleLimite(_,[]).
-cumpleLimite(P,S):- append(A1,[pieza(X,C_disp)|B1],P), append(A2,[pieza(X,1)|B2],S),
-					C_new is C_disp - 1,
+cumpleLimite(P,S):- append(A1,[pieza(X,C_disp)|B1],P), C_disp > 0, append(A2,[X|B2],S), C_new is C_disp - 1,
 					append(A1,[pieza(X,C_new)|B1],P2), append(A2,B2,S2), cumpleLimite(P2,S2), !.
-
 
 %%% Ejercicio 6
 
@@ -82,7 +84,9 @@ cumpleLimite(P,S):- append(A1,[pieza(X,C_disp)|B1],P), append(A2,[pieza(X,1)|B2]
 %  suman Total y, además, las cantidades utilizadas de cada pieza no exceden los declarados en Piezas.
 
 % Comentario: aprovecho las dos funciones anteriores.
-construir1(Tot,P,S):- generar(Tot,P,S), cumpleLimite(P,S).
+
+%construir1(Tot,P,S):- construido(Tot,P,S), !.
+construir1(Tot,P,S):- generar(Tot,P,S), cumpleLimite(P,S).%, asserta(construido(Tot,P,S)).
 
 
 % ####################################
@@ -93,9 +97,45 @@ construir1(Tot,P,S):- generar(Tot,P,S), cumpleLimite(P,S).
 
 % construir2(+Total,+Piezas,-Solución), cuyo comportamiento es id ́entico a construir1/3 pero que utiliza 
 %  definiciones dinámicas para persistir los cálculos auxiliares realizados y evitar repetirlos. 
-%  No se espera que las soluciones aparezcan en el mismo orden entre construir1/3 y construir2/3, pero sí, sean las mismas.
+%  No se espera que las soluciones aparezcan en el mismo orden entre construir1/3 y construir2/3, 
+%  pero sí, sean las mismas.
 
- construir2(_,_,_):- fail.
+:- dynamic dp/3. % dp(Tot,P,K,S) : en la solucion S se usan piezas de P de largo <= K para sumar Tot
+				 % llamo a funcrec con K = maximo valor de pieza que puedo utilizar.
+construir2(Tot,P,S) :- append(_,[pieza(Kmax,_)],P), funcrec(Tot,P,Kmax,S).
+funcrec(Tot,P,Kmax,S) :- funcrec2(Tot,P,Kmax,S), retractall(dp).
+
+% Comentario: funcion auxiliar: decrementar(Piezas,Sol,Res) quita de Piezas las piezas de los tamaños que
+% hay indicados en Sol (una lista de tamanios).
+% REQUIERE: Que en P haya una cantidad de piezas X >= que la cantidad de piezas X que hay en S
+decrementar([],[],[]).
+decrementar(P,[],P).
+decrementar([],_,[]).
+decrementar(P,[K|S],P2) :- append(A,[pieza(K,X)|B],P), X>1, Xm1 is X - 1, append(A,[pieza(K,Xm1)|B], Aux),
+						   decrementar(Aux,S,P2), !.
+decrementar(P,[K|S],P2) :- append(A,[pieza(K,1)|B],P), append(A,B,Aux), decrementar(Aux,S,P2), !.
+
+% Comentario:  si el tamanio maximo de las piezas es cero, entonces la lista vacia verifica
+% Si no hay piezas de tamnio K en P, llamo recursivamente con los mismos parametros, pero usando como 
+% maximo piezas de tamanio K-1.
+% Si hay al menos una pieza de tamanio K en P, pruebo todas las posiciones en las que podria
+% colocar tal ficha de tamanio K en la solucion (posiciones 0..Tot-K), hago P2 = P \ {K}, y llamo 
+% recursivamente a las subsolucion del intervalo de la izquierda: Asi obtengo una solucion S1.
+% Luego, modifico P3 = P2 \ {piezas utilizadas en S1}. Y llamo recursivamente con Tot - Tot2 - K y con
+% las fichas que quedaron disponibles en P3.
+% Ademas, cada vez que un llamado recursivo es exitoso, me guardo en "dynamic dp" la(s) solucion(es)
+% obtenida(s). 
+funcrec2(0,_,_,[]).
+funcrec2(Tot,_,K,S_k) :- dp(Tot,K,S_k).
+funcrec2(Tot,P,K,S_k) :- K > 0, not(member(pieza(K,_),P)), Km1 is K - 1, funcrec2(Tot,P,Km1,S_k),
+						 asserta(dp(Tot,Km1,S_k)).
+funcrec2(Tot,P,K,S_k) :- member(pieza(K,C),P), C > 0, Totmk is Tot - K, Totmk >= 0, Km1 is K - 1, 
+						 between(0,Totmk, Tot2), decrementar(P,[K],P2), funcrec2(Tot2, P2, Km1, S1),
+						 
+						 asserta(dp(Tot2,Km1,S1)), decrementar(P2,S1,P3), Tot3 is Tot - Tot2 - K, Tot3 >= 0,
+						 
+						 funcrec2(Tot3, P3, K, S2), asserta(dp(Tot3,K,S2)),
+						 append(S1,[K|S2],S_k), asserta(dp(Tot,K,S_k)), !.
 
 % ####################################
 % Comparación de resultados y tiempos
